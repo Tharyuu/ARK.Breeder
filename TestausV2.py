70dd905a36297741c3ec3de191393cab5cd6a304
Jukka V <126549684+jukkav1@users.noreply.github.com>
2023-03-21 15:37:39 +0200

    improved code and output readability

--- TestausV2.py
+++ TestausV2.py
@@ -1,15 +1,15 @@
-creature1=[]
-creature2=[]
+creature1 = []
+creature2 = []
 
 while True:
-    gender=input("Creature gender:")
-    healt=input("Creature healt:")
-    stamina=input("Creature stamina:")
-    weight=input("Creature weight:")
-    melee=input("Creature melee:")
-    print("")
-    
-    if creature1==[]:
+    gender = input("Creature gender: ")
+    healt = input("Creature healt: ")
+    stamina = input("Creature stamina: ")
+    weight = input("Creature weight: ")
+    melee = input("Creature melee: ")
+    print()
+
+    if creature1 == []:
         creature1.append(healt)
         creature1.append(stamina)
         creature1.append(weight)
@@ -22,43 +22,43 @@ while True:
         creature2.append(melee)
         break
 
-kierros=0
+kierros = 0
 
 for stat in creature1:
-    if kierros==0:
-        print(f"Healt:{stat}")
-    elif kierros==1:
-        print(f"Stamina:{stat}")
-    elif kierros==2:
-        print(f"Weight:{stat}")
-    elif kierros==3:
-        print(f"Melee:{stat}\n")
-    kierros+=1
-    
-kierros=0
-for stat in creature2:
-    if kierros==0:
-        print(f"Healt:  {stat}")
-    elif kierros==1:
-        print(f"Stamina:{stat}")
-    elif kierros==2:
-        print(f"Weight: {stat}")
-    elif kierros==3:
-        print(f"Melee:  {stat}\n")
-    kierros+=1
+    if kierros == 0:
+        print(f"Healt:\t {stat}")
+    elif kierros == 1:
+        print(f"Stamina: {stat}")
+    elif kierros == 2:
+        print(f"Weight:\t {stat}")
+    elif kierros == 3:
+        print(f"Melee:\t {stat}\n")
+    kierros += 1
 
-word="Healt"
-for min,max in zip(creature1,creature2):
-    if max<min:
+kierros = 0
+for stat in creature2:
+    if kierros == 0:
+        print(f"Healt:\t {stat}")
+    elif kierros == 1:
+        print(f"Stamina: {stat}")
+    elif kierros == 2:
+        print(f"Weight:\t {stat}")
+    elif kierros == 3:
+        print(f"Melee:\t {stat}\n")
+    kierros += 1
+
+word = "Healt"
+for min, max in zip(creature1, creature2):
+    if max < min:
         print(f"{word} {min} creature1")
-    elif max>min:
+    elif max > min:
         print(f"{word} {max} creature2")
     else:
         print(f"{word} {max} same")
-    
-    if word=="Healt":
-        word=word.replace("Healt", "Stamina")
-    elif word=="Stamina":
-        word=word.replace("Stamina","Weight")
+
+    if word == "Healt":
+        word = word.replace("Healt", "Stamina")
+    elif word == "Stamina":
+        word = word.replace("Stamina", "Weight")
     else:
-        word=word.replace("Weight","Melee")
+        word = word.replace("Weight", "Melee")
